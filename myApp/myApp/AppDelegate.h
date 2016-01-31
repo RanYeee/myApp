d17108f873f8129ba5e5bbbbd4d422a330d472f4
RanYeee <xar93@qq.com>
2016-01-31 13:49:44 +0800

    first import

---
+++ myApp/myApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myApp
+//
+//  Created by IMac on 16/1/31.
+//  Copyright © 2016年 IMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myApp/myApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myApp
+//
+//  Created by IMac on 16/1/31.
+//  Copyright © 2016年 IMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
